02203410368614cbfe4ee24ed36c3cf8bf4c6a03
Louhwz <872188659@qq.com>
2020-02-26 21:24:32 +0800

    添加了一种新的算法，利用二进制+位运算替代递归。

--- Algorithm/78.子集.cpp
+++ Algorithm/78.子集.cpp
@@ -21,15 +21,31 @@ public:
         }
         return;
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsetsRecursion(vector<int>& nums) {
         vector<int> path{};    
         sSize = nums.size();
         for (int i = 0; i <= sSize; i++)
             traceBack(nums, path, i, 0);
         return res;
     }
+
+    //二进制+位运算，集合的每个元素都可以选或者不选
+    vector<vector<int> >subsets(vector<int>& nums) {
+        vector<vector<int> >res;
+        for (int i = 0; i < (1 << nums.size()); i++) {
+            vector<int> path{};
+            for (int j = 0; j < nums.size(); j++) {
+                if (((i >> j) & 1) == 1)
+                    path.push_back(nums[j]);
+            }
+            res.push_back(path);
+        }
+        return res;
+    }
 };
 int main() {
-    vector<vector<int> > res{};
-    cout << res.size();
+    //vector<vector<int> > res{};
+    vector<int> s{ 1,2,3 };
+    cout << (0 >> 1);
+    //cout << res.size();
 }
